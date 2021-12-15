f9cf39c33e24f632493d042df62ac596a59f7e92
alexaoh <alexaoh@users.noreply.github.com>
2021-12-15 16:35:50 +0100

    Began working on Task2 in SPP and making a RMD (one file)

--- spp/Example3.R
+++ spp/Example3.R
@@ -1,45 +1,41 @@
 library(spatstat)
 
-setwd("/home/ajo/gitRepos/spatial/spp")
-
 # Empty space function
 data(cells)
 ?cells
 plot(cells)
 class(cells)
 
-# Empty space function. 
 Fc <- Fest(cells)
 Fc
 par(pty = "s")
-plot(Fest(cells,correction="best"),legend=T)
+plot(Fest(cells,correction="best"),legend=F)
 
-# NN (Nearest Neighbor) function
+# NN function
 Gc <- Gest(cells)
 Gc
 par(pty = "s")
-plot(Gest(cells,correction="best"),legend=T)
+plot(Gest(cells,correction="best"),legend=F)
 
 # Pairwise distances
 K <- Kest(cells)
 K
 par(pty = "s")
-plot(Kest(cells,correction="best"),legend=T)
+plot(Kest(cells,correction="best"),legend=F)
 
 
 # Envelope
 
 E <- envelope(cells, Kest, nsim = 39,correction="best")
 E
-plot(E, main = "pointwise envelopes",legend=T)
+plot(E, main = "pointwise envelopes",legend=F)
 
 E <- envelope(cells, Kest, nsim = 19, rank = 1, global = TRUE,correction="best")
 E
-plot(E, main = "global envelopes",legend=T)
+plot(E, main = "global envelopes",legend=F)
 
-# This is the test with more power, as shown in slides (Lest is used instead of Kest).
 E <- envelope(cells, Lest, nsim = 19, rank = 1, global = TRUE,correction="best")
-plot(E, main = "global envelopes of L(r)", legend=T)
+plot(E, main = "global envelopes of L(r)", legend=F)
 
 
 
@@ -76,48 +72,83 @@ plot(fitM,legend=F,what="statistic")
 Xsim=rMatClust(fitM$par[1],fitM$par[2],fitM$mu,win=redwood$window)
 plot(Xsim)
 
+qunif(c(0.95),0,0.0865)
+
 # Log-Gaussian Cox process
 library(RandomFields)
 fitLG <- kppm(redwood, ~1, "LGCP")
+fitLG
 
 plot(fitLG,legend=F,what="statistic")
 
+fitLG$par
+sLG<-fitLG$par[1]
+aLG<-fitLG$par[2]
 
-r<-0.1
-1.048*exp(-(r/0.1))
+# Covariance at distance 0.1
+sLG*exp(-(0.1/aLG))
 
+#Autocorrelation function
 r<-seq(0,1,0.01)
-plot(r,exp(-(r/0.1)),type="l")
+plot(r,exp(-(r/aLG)),type="l")
 
+# Range with autocorrelation = 0.2
+aLG*log(0.2)*(-1)
 
 
 Xsim=rLGCP("exp", mu = fitLG$modelpar[3], var=fitLG$modelpar[1],scale=fitLG$modelpar[2],win=redwood$window)
 plot(Xsim)
 
 
-# Gaussian. It takes too much time
+# Gaussian. It took 32s with i7-8565U CPU @ 1.80GHz 8th Gen  + 16Gb RAM
 date()
 fitLG2 <- kppm(redwood, ~1, "LGCP",covmodel=list(model="gauss"))
 date()
 fitLG2
 
-r<-0.1
-0.929*exp(-(r/0.108))
+plot(fitLG2,legend=F,what="statistic")
+plot(fitLG,add=T,lty=2)
 
+fitLG2$par
+sLG2<-fitLG2$par[1]
+aLG2<-fitLG2$par[2]
+
+# Covariance at distance 0.1
+sLG2*exp(-(0.1/aLG2))
+
+#Autocorrelation function
 r<-seq(0,1,0.01)
-plot(r,exp(-(r/0.108)^2),type="l")
+plot(r,exp(-(r/aLG2)^2),type="l")
+lines(r,exp(-(r/aLG)),type="l",lty=2)
+
 
 # Faster
 fitLG2b <- kppm(redwood, ~1, "LGCP",covmodel=list(model="gauss"),method="palm")
-fitLG2b
+
+plot(fitLG2b,add=T,lty=3)
+
+fitLG2b$par
+sLG2b<-fitLG2b$par[1]
+aLG2b<-fitLG2b$par[2]
+
+# Covariance at distance 0.1
+sLG2b*exp(-(0.1/aLG2b))
+
+
+#Autocorrelation function
+r<-seq(0,1,0.01)
+plot(r,exp(-(r/aLG2)),type="l")
+lines(r,exp(-(r/aLG)),type="l",lty=2)
+lines(r,exp(-(r/aLG2b)),type="l",lty=3)
 
 
 
-# The three models together (only available for minimum contrast method)
+
+# The four models together (only available for minimum contrast method)
 plot(fit,what="statistic",legend=F)
-lines(fitM$Fit$Stat$r,fitM$Fit$mcfit$fit$fit)
-lines(fitLG$Fit$Stat$r,fitLG$Fit$mcfit$fit$fit)
-lines(fitLG2$Fit$Stat$r,fitLG$Fit$mcfit$fit$fit)
+lines(fitM$Fit$Stat$r,fitM$Fit$mcfit$fit$fit,col="red")
+lines(fitLG$Fit$Stat$r,fitLG$Fit$mcfit$fit$fit,col="green")
+lines(fitLG2$Fit$Stat$r,fitLG2$Fit$mcfit$fit$fit,col="blue")
 
 # Compare exp vs gauss
 fitLG3 <- kppm(redwood, ~1, "LGCP",method="palm")
@@ -127,11 +158,13 @@ AIC(fitLG3)
 
 
 # Tolerance bands
-E <- envelope(fit, Lest, nsim = 19, nrank = 1, global = TRUE,correction="best")
+E <- envelope(fit, Lest, nsim = 19, nrank = 1, 
+              global = TRUE,correction="best")
 plot(E, main = "global envelopes of L",legend=F)
 
-E <- envelope(fitLG2, Lest, nsim = 19, nrank = 1, global = TRUE,correction="best")
-plot(E, main = "global envelopes of L")
+E <- envelope(fitLG, Lest, nsim = 19, nrank = 1, 
+              global = TRUE,correction="best")
+plot(E, main = "global envelopes of L",legend=F)
 
 
 ##################
@@ -167,16 +200,27 @@ pfit=profilepl(df,Geyer,redwood,~1)
 pfit
 plot(pfit)
 
+# Set r=0.1
+df=data.frame(r=rep(0.1,11),sat=2:12)
+pfit=profilepl(df,Geyer,redwood,~1)
+pfit
+plot(pfit)
 
 fitG=ppm(redwood, ~1, Geyer(r = 0.1, sat = 7))
 fitG
 plot(fitG)
 
-E <- envelope(fitG, Lest, nsim = 19, rank = 1, global = TRUE,correction="best")
+fitG2=ppm(redwood, ~1, Geyer(r = 0.1, sat = 4))
+fitG2
+plot(fitG)
+AIC(fitG)
+AIC(fitG2)
+
+E <- envelope(fitG2, Lest, nsim = 19, rank = 1, global = TRUE,correction="best")
 plot(E, main = "global envelopes of L(r)",legend=F)
 
 AIC(fitA)
-AIC(fitG)
+AIC(fitG2)
 
 
 ## Regular process
@@ -240,12 +284,7 @@ plot(n84p,main="nucli84")
 axis(1,at=c(seq(-25,100,by=25)),pos=c(-75,0))
 axis(2,at=c(seq(-75,100,by=25)),pos=c(-25,0))
 
-# Envelope
-E <- envelope(n84p, Lest, nsim = 39, rank = 1,correction="none")
-plot(E, main = "pointwise envelopes")
 
-E <- envelope(n84p, Lest, nsim = 19, rank = 1, global = TRUE,correction="none")
-plot(E, main = "global envelopes of L(r)")
 
 # Covariables: height and vegetation
 
@@ -269,7 +308,6 @@ plot(n84p, add=T, cex=0.5)
 fit <- ppm(n84p, ~height + veg)
 lam <- predict(fit, locations = n84p)
 Ki <- Kinhom(n84p, lam,correction="none")
-Ki
 plot(Ki, main = "Inhomogeneous K function",legend=F)
 
 # Envelope
@@ -280,15 +318,18 @@ E <- envelope(fit, Lest, nsim = 19, rank = 1, global = TRUE,correction="none")
 plot(E, main = "global envelopes of L(r)")
 
 
-
-
 # Models
 
 #Thomas
 fit <- kppm(n84p, ~height + veg, "Thomas")
 fit
-summary(fit)
+
 plot(fit, what="statistic")
+plot(fit)
+
+plot(predict(fit))
+plot(n84p, add = TRUE, pch = "+")
+
 
 # Envelope
 E <- envelope(fit, Lest, nsim = 39, rank = 1,correction="none")
@@ -302,15 +343,10 @@ plot(E, main = "global envelopes of L(r)")
 Xsim=rThomas(fit$par[1], sqrt(fit$par[2]), fit$mu, win=n84p$window)
 plot(Xsim)
 
-plot(predict(fit))
-plot(n84p, add = TRUE, pch = "+")
-
-
 
 # Matern
 fitM <- kppm(n84p, ~height + veg, "MatClust")
 fitM
-summary(fitM)
 plot(fitM,what="statistic")
 
 
@@ -329,11 +365,22 @@ plot(n84p, add = TRUE, pch = "+")
 # Log Gaussian Cox
 fitLG <- kppm(n84p, ~height+veg, "LGCP")
 fitLG
-summary(fitLG)
 
 plot(fitLG,what="statistic")
-
 plot(fitLG)
+
+sLG<-fitLG$par[1]
+aLG<-fitLG$par[2]
+
+# Covariance at distance 10
+sLG*exp(-(6/aLG))
+
+#Autocorrelation function
+r<-seq(0,20,1)
+plot(r,exp(-(r/aLG)),type="l")
+
+# Range with autocorrelation = 0.2
+aLG*log(0.2)*(-1)
 
 # simulation
 
@@ -343,23 +390,23 @@ trend=as.im(log(predict(fitLG)),W=n84p$window)
 Xsim=rLGCP("exp", mu =trend, var=fitLG$modelpar[1],scale=fitLG$modelpar[2],win=n84p$window)
 plot(Xsim)
 
-
-
-
 E <- envelope(fitLG, Lest, nsim = 19, rank = 1, global = TRUE,correction="none",
               simulate=expression(Xsim=rLGCP("exp", mu = trend, var=fitLG$modelpar[1],scale=fitLG$modelpar[2],win=n84p$window)
 ))
-plot(E, main = "global envelopes of L(r)")
+
+plot(E, main = "global envelopes of L(r)", legend=F)
 
 
 
 # Gibbs process
 
-
 # Area Interaction
-# It takes some time
+# It takes some time 80s
 df=data.frame(r=seq(3,6,by=0.5))
-pfit=profilepl(df,AreaInter,n84p,~height+veg,correction="none")
+date()
+pfit=profilepl(df,AreaInter,n84p,~height+veg,
+               correction="none")
+date()
 pfit
 plot(pfit)
 
@@ -371,13 +418,24 @@ E <- envelope(fitA, Lest, nsim = 19, rank = 1, global = TRUE,correction="none")
 plot(E, main = "global envelopes of L(r)")
 
 
-
 # Geyer
-df=data.frame(r=seq(0.5,10,by=0.5),sat=1:20)
+df=data.frame(r=seq(4,15,by=0.5),sat=1:23)
 pfit=profilepl(df,Geyer,n84p,~height+veg,correction="none")
 pfit
 plot(pfit)
-fitG=ppm(n84p, ~height+veg, Geyer(r = 5.5, sat = 11))
+
+sat<-2:7
+r<-5:10
+
+library(tidyr)
+
+df=as.data.frame(crossing(r,sat))
+pfit=profilepl(df,Geyer,n84p,~height+veg,correction="none")
+pfit
+plot(pfit)
+
+
+fitG=ppm(n84p, ~height+veg, Geyer(r = 6, sat = 7))
 fitG
 summary(fitG)
 plot(fitG)
@@ -391,3 +449,25 @@ plot(E, main = "global envelopes of L(r)")
 
 AIC(fitA)
 AIC(fitG)
+
+
+fitG2=ppm(n84p, ~height, Geyer(r = 6, sat = 7))
+AIC(fitG2)
+summary(fitG2)
+plot(fitG2)
+
+
+#Smoothed residuals
+#Smoothed residuals
+diagnose.ppm(fitG2, which = "smooth",type="pearson",rbord=0)
+
+# Null standard deviation = 1/(2*bandwidth*sqrt(pi))
+0.0158*2
+
+
+# Lurking variable plot
+lurking(fitG2, expression(x), type = "raw",envelope=T)
+lurking(fitG2, expression(y), type = "raw",envelope=T)
+lurking(fitG2, height, type = "raw",envelope=T)
+
+
